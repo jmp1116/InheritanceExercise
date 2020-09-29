5610aa7f9c786ae2ed708f4a76471f5a6e0d9052
John Mark <jmp1116@gmail.com>
2020-09-28 20:25:10 -0500

    finished exercise

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -7,33 +7,39 @@ namespace Inheritance
         static void Main(string[] args)
         {
             // TODO Be sure to follow best practice when creating your classes
+            Bird bird = new Bird();
+            Reptile reptile = new Reptile();
 
-            // Create a class Animal
-            // give this class 4 members that all Animals have in common
+            reptile.HardShells = true;
+            reptile.HaveScales = true;
+            reptile.LayEggs = true;
+            reptile.LiveOnLand = true;
+            reptile.NumberOfEyes = 2;
+            reptile.GotHair = false;
+            reptile.ColdBlooded = true;
+            reptile.AreVertabrates = true;
+            reptile.NumberOfExtremenies = 4;
 
+            bird.FlySouth = true;
+            bird.DoesFly = true;
+            bird.GotHair = false;
+            bird.HaveFeathers = true;
+            bird.HollowBones = true;
+            bird.LayEggs = true;
+            bird.LiveOnLand = true;
+            bird.NumberOfExtremenies = 4;
+            bird.NumberOfEyes = 2;
 
-            // Create a class Bird
-            // give this class 4 members that are specific to Bird
-            // Set this class to inherit from your Animal Class
+            Console.WriteLine("Do Birds lay eggs?:", bird.LayEggs);
+            Console.WriteLine("Do Reptiles lay eggs?:", reptile.LayEggs);
+            Console.WriteLine("Do Birds live on land?:", bird.LiveOnLand);
+            Console.WriteLine("Do Reptiles live on land?:", reptile.LiveOnLand);
+            Console.WriteLine("How many eyes do birds have?:", bird.NumberOfEyes);
+            Console.WriteLine("How many eyes do reptiles have?:", reptile.NumberOfEyes);
+            Console.WriteLine("Do birds have hair?:", bird.GotHair);
+            Console.WriteLine("Do reptiles have hair?:", reptile.GotHair);
+            Console.WriteLine("Do birds fly?:", bird.DoesFly);
 
-            // Create a class Reptile
-            // give this class 4 members that are specific to Reptile
-            // Set this class to inherit from your Animal Class
-
-
-
-
-            /*Create an object of your Bird class
-             *  give values to your members using the object of your Bird class
-             *  
-             * Creatively display the class member values 
-             */
-
-            /*Create an object of your Reptile class
-             *  give values to your members using the object of your Bird class
-             *  
-             * Creatively display the class member values 
-             */
         }
     }
 }
